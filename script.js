d708585d4b04a070408615c80cc75a6b7fb9eb88
ahsan-ali49 <ahsanali.bscssef20@iba-suk.edu.pk>
2023-02-03 15:22:52 +0500

    refactored code

--- script.js
+++ script.js
@@ -1,54 +1,64 @@
+function getCellNumber() {
+  let order;
+
+  do {
+    order = prompt("Enter cell's order(<101): ");
+  } while (order > 100 || order == null);
+  return order;
+}
+
+function createDiv(order) {
+  let gridDiv;
+  let numOfCells = order * order;
+  for (let i = 0; i < numOfCells; i++) {
+    gridDiv = document.createElement("div");
+    gridDiv.className = "grid-cell";
+    const widh = 400 / order;
+    gridDiv.style.width = `${widh}px`;
+    container.appendChild(gridDiv);
+  }
+}
+
+function initPage() {
+  let order = getCellNumber();
+
+  createDiv(order);
+
+  body.appendChild(container);
+
+  const gridCells = document.querySelectorAll(".grid-cell");
+
+  gridCells.forEach((cell) => {
+    cell.addEventListener("mouseenter", () => {
+      const colors = [
+        "black",
+        "silver",
+        "gray",
+        "maroon",
+        "red",
+        "purple",
+        "fuchsia",
+        "green",
+        "lime",
+        "olive",
+        "yellow",
+        "navy",
+        "blue",
+        "teal",
+        "aqua",
+      ];
+      cell.style.backgroundColor = `${
+        colors[Math.floor(Math.random() * colors.length)]
+      }`;
+    });
+  });
+}
+
 const body = document.querySelector("body");
 
 const container = document.createElement("div");
 container.className = "main-container";
 
-let order;
-let numOfCells;
+const promptBtn = document.querySelector(".promptBtn");
 
-do {
-  order = prompt("Enter cell's order(<101): ");
-  numOfCells = order * order;
-} while (order > 100);
-
-let gridDiv;
-// gridDiv.className = "grid-div";
-
-for (let i = 0; i < numOfCells; i++) {
-  gridDiv = document.createElement("div");
-  gridDiv.className = "grid-div";
-  const widh = 400 / order;
-  gridDiv.style.width = `${widh}px`;
-  container.appendChild(gridDiv);
-}
-
-body.appendChild(container);
-
-const gribular = document.querySelectorAll(".grid-div");
-
-// addEventListener("mouseenter")
-
-gribular.forEach((grib) => {
-  grib.addEventListener("mouseenter", () => {
-    const colors = [
-      "black",
-      "silver",
-      "gray",
-      "maroon",
-      "red",
-      "purple",
-      "fuchsia",
-      "green",
-      "lime",
-      "olive",
-      "yellow",
-      "navy",
-      "blue",
-      "teal",
-      "aqua",
-    ];
-    grib.style.backgroundColor = `${
-      colors[Math.floor(Math.random() * colors.length)]
-    }`;
-  });
-});
+promptBtn.addEventListener("click", initPage);
